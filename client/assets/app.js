2c28f2758556c5967a76f2baf4a1c148e60a5cd0
hermanj13 <hermanj13@me.com>
2017-05-24 13:06:00 -0400

    changed anchor on url

--- client/assets/app.js
+++ client/assets/app.js
@@ -1,6 +1,7 @@
 var app = angular.module('myApp', ['ngRoute']);
-app.config(function($routeProvider) {
+app.config(function($routeProvider, $locationProvider) {
   $routeProvider
     .when('/', { templateUrl: 'partials/home.html', })
     .otherwise({ redirectTo: '/' });
+  $locationProvider.html5Mode(true);
 });
